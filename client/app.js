29921d93b20a5def1fc2a27749034c121db6459a
Eduardo Turcios <eddieturcios@outlook.com>
2022-06-18 20:42:36 -0400

    inital render of react componets

--- client/app.js
+++ client/app.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+
+const root = createRoot(container);
+root.render(<h1>Hello World</h1>);
