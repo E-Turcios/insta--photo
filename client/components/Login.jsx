339fccc4a7fabfb6cfbb2faeb7c971af06a3c24a
Eduardo Turcios <eddieturcios@outlook.com>
2022-06-21 01:58:30 -0400

    feat: added input to login

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Stack,
+  InputRightElement,
+  Button,
+  InputGroup,
+} from '@chakra-ui/react';
 
 export default function Login() {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
   return (
-    <h1>login</h1>
-  )
+    <Stack width="25%">
+      <FormControl>
+        <FormLabel>Email</FormLabel>
+        <Input type="email" id="email" placeholder="johndoe@gmail.com"></Input>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? 'text' : 'password'}
+            id="password"
+            placeholder="johndoe@gmail.com"
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+    </Stack>
+  );
 }
