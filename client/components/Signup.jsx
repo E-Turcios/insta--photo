217f88e7a25e561fa5405ba0f66b3ff452add06d
boundy99 <abdoulaye.djikine@stu.bmcc.cuny.edu>
2022-06-22 00:44:12 -0400

    Sign Up Page

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -13,18 +13,54 @@ import {
 } from '@chakra-ui/react';
 
 export default function Signup() {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
   return (
-    <FormControl>
-      <FormLabel htmlFor="email"> First Name</FormLabel>
-      <Input id="firstname" type="text" />
-    </FormControl>
-    <FormControl>
-    <FormLabel htmlFor="email"> Last Name</FormLabel>
-    <Input id="lastname" type="text" />
-  </FormControl>
-  <FormControl>
-  <FormLabel htmlFor='email'>Email address</FormLabel>
-  <Input id='email' type='email' />
-  </FormControl>
+    <>
+      <FormControl>
+        <FormLabel> First Name</FormLabel>
+        <Input id="firstname" type="text" />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Middle Name</FormLabel>
+        <Input id="middlename" type="text" />
+        <FormHelperText>Optional</FormHelperText>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel> Last Name</FormLabel>
+        <Input id="lastname" type="text" />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Email address</FormLabel>
+        <Input id="email" type="email" />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input id="password" type={show ? 'text' : 'password'} />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+
+      </FormControl>
+      <FormControl>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup>
+          <Input id="confirmationpassword" type={show ? 'text' : 'password'} />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+    </>
   );
 }
