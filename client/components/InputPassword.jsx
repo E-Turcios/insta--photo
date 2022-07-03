8579b910f33fdf5d5c1b5a3cf5b4bf987bdc19b5
Eduardo Turcios <eddieturcios@outlook.com>
2022-07-03 16:02:38 -0400

    feat: added a placeholder to the input for password

--- client/components/InputPassword.jsx
+++ client/components/InputPassword.jsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
-import { FormControl, InputGroup, Input, InputRightElement, Button, useBoolean } from '@chakra-ui/react';
+import {
+  FormControl,
+  InputGroup,
+  Input,
+  InputRightElement,
+  Button,
+  useBoolean,
+} from '@chakra-ui/react';
 
-const InputPassword = React.forwardRef((props, passwordRef) => {
+const InputPassword = React.forwardRef(({placeholder}, passwordRef) => {
   const [hasInput, setHasInput] = useState(false);
   const [show, setShow] = useBoolean(false);
   function handlePasswordInput(event) {
     setHasInput(event.target.value.length);
   }
+
   return (
     <FormControl>
       <InputGroup>
         <Input
-          id="password"
-          placeholder="Password"
+          placeholder={placeholder || 'Password'}
           type={show ? 'text' : 'password'}
           ref={passwordRef}
           onInput={event => handlePasswordInput(event)}
