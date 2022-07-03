429b983165a4d30025625bbad61ca5abefdf2226
boundy99 <abdoulaye.djikine@stu.bmcc.cuny.edu>
2022-07-03 01:16:00 -0400

    Main Heading Font

---
+++ client/Theme/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+import "@fontsource/bellota"
+
+const theme = extendTheme({
+    fonts: {
+        heading: 'Bellota'
+    },
+})
+
+export default theme
--- webpack.config.js
+++ webpack.config.js
@@ -4,38 +4,47 @@ const { resolve } = require('path');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './client/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: resolve(__dirname, 'public'),
-    publicPath: '/',
-  },
-  devServer: {
-    port: 8080,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-  },
-  mode: 'development',
-  context: __dirname,
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
+    entry: './client/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'public'),
+        publicPath: '/',
+    },
+    devServer: {
+        port: 8080,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+    },
+    mode: 'development',
+    context: __dirname,
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            // {
+            //     test: /.(png|woff|woff2|eot|ttf|svg)$/,
+            //     use: [
+            //         { loader: 'url-loader', options: { limit: 10000, } }
+            //     ]
+            // },
+            {
+                test: /.css$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new Dotenv({
+            path: './.env.development.local',
+        }),
     ],
-  },
-  plugins: [
-    new Dotenv({
-      path: './.env.development.local',
-    }),
-  ],
-};
+}
