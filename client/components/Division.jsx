b3286a5dea1c8c57e7295d683c9f63d2813dc080
Eduardo Turcios <eddieturcios@outlook.com>
2022-07-03 18:45:59 -0400

    refac: removed color from divider

--- client/components/Division.jsx
+++ client/components/Division.jsx
@@ -3,7 +3,7 @@ import { Flex, Divider, Text } from '@chakra-ui/react';
 export default function Division({ children }) {
   return (
     <Flex align="center">
-      <Divider colorScheme="gray.200" />
+      <Divider />
       <Text fontSize="xs" padding="2" color="gray">
         {children}
       </Text>
