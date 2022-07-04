7d6416be073b010d18f840c6371aaab5266a3d7c
Eduardo Turcios <eddieturcios@outlook.com>
2022-07-03 22:41:52 -0400

    dev: added env port and mode into webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,43 @@
 
 const { resolve } = require('path');
 const Dotenv = require('dotenv-webpack');
-
 module.exports = {
-    entry: './client/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: resolve(__dirname, 'public'),
-        publicPath: '/',
-    },
-    devServer: {
-        port: 8080,
-        open: true,
-        hot: true,
-        historyApiFallback: true,
-    },
-    mode: 'development',
-    context: __dirname,
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    module: {
-        rules: [{
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /.css$/i,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-        ],
-    },
-    plugins: [
-        new Dotenv({
-            path: './.env.development.local',
-        }),
+  entry: './client/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'public'),
+    publicPath: '/',
+  },
+  devServer: {
+    port: process.env.PORT,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+  mode: 'development',
+  context: __dirname,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /.css$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
-}
+  },
+  plugins: [
+    new Dotenv({
+      path: `./.env.${process.env.MODE}.local`,
+    }),
+  ],
+};
