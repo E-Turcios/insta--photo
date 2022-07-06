6ab3dec88ef87e4ba5d1001842e11cc97d53f203
Eduardo Turcios <eddieturcios@outlook.com>
2022-07-06 15:09:11 -0400

    feat: added file loder to load images

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,14 @@ module.exports = {
         test: /.css$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
